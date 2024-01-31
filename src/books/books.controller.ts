9d45450c314bc3b9de3d142861627b8af75e0def
JubaerHosain <jubaerhosain1119@gmail.com>
2024-01-31 18:16:58 +0600

    updated

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
@@ -22,18 +22,17 @@ export class BooksController {
     }
 
     @Get(':id')
-    findOne(@Param('id', ) id: number): Promise<BookDto> {
-        console.log(id);
+    findOne(@Param('id', ParseIntPipe) id: number): Promise<BookDto> {
         return this.booksService.findOne(id);
     }
 
     @Patch(':id')
-    update(@Param('id') id: number, @Body() updateBookDto: UpdateBookDto): Promise<BookDto> {
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateBookDto: UpdateBookDto): Promise<BookDto> {
         return this.booksService.update(id, updateBookDto);
     }
 
     @Delete(':id')
-    remove(@Param('id') id: number) {
+    remove(@Param('id', ParseIntPipe) id: number) {
         return this.booksService.remove(+id);
     }
 }
