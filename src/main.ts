7d9fdaf58d2edc45f9086c3d48a4cc0587ab27bf
JubaerHosain <jubaerhosain1119@gmail.com>
2024-02-03 17:20:42 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ValidationExceptionFactory } from './exceptions/validation.exception';
+import { NestedValidationExceptionFactory } from './exceptions/validation.exception';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -11,7 +11,7 @@ async function bootstrap() {
             transform: true,
             whitelist: true,
             forbidNonWhitelisted: true,
-            exceptionFactory: ValidationExceptionFactory,
+            exceptionFactory: NestedValidationExceptionFactory,
         }),
     );
     await app.listen(process.env.PORT);
